0aea83f1ffbbaff51315a21fd6e3ae7db1141f44
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2023-10-22 06:40:03 +0100

    added results for all

--- OOP/math_no_constructor.js
+++ OOP/math_no_constructor.js
@@ -13,12 +13,17 @@ class MathHelper{
     }
 
     subtract(num1, num2) {
-        return num1 * num2
+        return num1 - num2
     }
 
 }
 
 const mathHelper = new MathHelper()
-const res = mathHelper.div(500, 50)
+const resDiv = mathHelper.div(500, 50)
+const resAdd = mathHelper.add(300, 250)
+const resSub = mathHelper.subtract(900, 450)
 
-console.log(res)
+
+console.log(resDiv)
+console.log(resAdd)
+console.log(resSub)
